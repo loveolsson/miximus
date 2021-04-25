1bdcf2218711288e271cf07a991f8f017b7cccb6
Love Olsson <love.olsson@gmail.com>
2021-04-25 06:16:53 +0200

    Gzipping web files and unziping them on app start

--- static/include/static_files/files.hpp
+++ static/include/static_files/files.hpp
@@ -1,7 +1,7 @@
 #pragma once
+#include <string>
 #include <unordered_map>
-#include <string_view>
 
 namespace miximus::static_files {
-    extern const std::unordered_map<std::string_view, std::string_view> files;
+extern const std::unordered_map<std::string, std::string> webFiles;
 }
--- static/web/bundler/bundler.js
+++ static/web/bundler/bundler.js
@@ -1,10 +1,12 @@
-const { promisify } = require('util');
-const { resolve, relative } = require('path');
+const {promisify} = require('util');
+const {resolve, relative} = require('path');
 const fs = require('fs');
+const {EOL} = require('os');
+const {gzipSync} = require('zlib');
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
-const {EOL} = require('os');
 
+// Recursively get all files in a directory
 async function getFiles(dir) {
   const subdirs = await readdir(dir);
   const files = await Promise.all(subdirs.map(async (subdir) => {
@@ -14,40 +16,60 @@ async function getFiles(dir) {
   return files.reduce((a, f) => a.concat(f), []);
 }
 
-getFiles('./dist').then(files => {
-    let data = '#include "static_files/files.hpp"' + EOL;
-    data += 'namespace miximus::static_files {' + EOL;;
+// Get all files in the 'dist' folder
+getFiles('./dist').then((files) => {
+  // Add the header of the file
+  let data = '#include "static_files/files.hpp"' + EOL;
+  data += '#include <gzip/decompress.hpp>' + EOL + EOL;
+  data += 'namespace miximus::static_files {' + EOL;
 
-    let map = 'const std::unordered_map<std::string_view, std::string_view> files = {' + EOL;;
+  // Create the declaration of the map containing the uncompressed files
+  let map =
+      'const std::unordered_map<std::string, std::string> webFiles = {' + EOL;
 
-    files.forEach((file, index) => {
-        const rel = relative('./dist', file);
-        const buf = fs.readFileSync(file);
-        
-        data += `// File: ${rel}` + EOL;
-        data += `static const uint8_t fileData${index}[] = {` + EOL;
-        for (let i = 0; i < buf.length; ) {
-            data += '\t'
-            for (let j = 0; j < 20 && i < buf.length; ++j, ++i) {
-                data += `0x${buf[i].toString(16).padStart(2, '0')}, `;
-            }
-            data += '' + EOL;
-        }
-        data += '};' + EOL;
+  // Iterate the files in the folder
+  files.forEach((file, index) => {
+    // Get the relative path
+    const rel = relative('./dist', file).replace('\\', '/');
 
-        map += `\t{ std::string_view("${rel.replace('\\', '/')}"), std::string_view((const char*)fileData${index}, (size_t)${buf.length}) },` + EOL;
-    });
+    // Read the contents of the file into a buffer
+    let buf = fs.readFileSync(file);
 
-    map += '};' + EOL;;
+    // Compress the buffer
+    buf = gzipSync(buf);
 
-    data += map;
-
-    data += '} // namespace miximus::static_files' + EOL;;
-
-    const dir = './dist-bundle';
-    if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
+    // Print the contents of the buffer
+    data += `// File: ${rel}` + EOL;
+    data += `static const uint8_t fileData${index}[] = {` + EOL;
+    for (let i = 0; i < buf.length;) {
+      data += '\t';
+      // Add the bytes in rows of 20
+      for (let j = 0; j < 20 && i < buf.length; ++j, ++i) {
+        data += `0x${buf[i].toString(16).padStart(2, '0')}, `;
+      }
+      data += '' + EOL;
     }
-    fs.writeFileSync(resolve(dir,'dist.cpp'), data);
-});
+    data += '};' + EOL + EOL;
 
+    // Add an entry to the map that decompresses the file into the map
+    map += `\t{ "${rel}", gzip::decompress((const char*)fileData${index}, ${
+               buf.length}) },` +
+        EOL;
+  });
+
+  // Terminate the map declaration and add it to the file
+  map += '};' + EOL + EOL;
+  data += map;
+
+  // Add termination of the namespace to the file
+  data += '} // namespace miximus::static_files' + EOL;
+
+  // Create the folder if it doesn't exist
+  const dir = './dist-bundle';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  // Write the file buffer to disk
+  fs.writeFileSync(resolve(dir, 'dist.cpp'), data);
+});
